077935a0e98bbfe9510bc7300f591219db4efa40
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-11-22 15:02:19 +0200

    Added tests

--- exercises-2/main.go
+++ exercises-2/main.go
@@ -1,49 +1,53 @@
 package main
 
-import "fmt"
-
 type node[T any] struct {
-	v           T
+	val         T
 	left, right *node[T]
 }
 
-// Outputs the way from target to root
-// *Return value is for internal use only
-func getPath[T any](root, target *node[T], sink func(n *node[T])) bool {
-	if root == nil {
-		return false
-	}
-	if root == target || getPath(root.left, target, sink) || getPath(root.right, target, sink) {
-		sink(root)
-		return true
-	}
-	return false
-}
-
-// NCA stands for Nearest Common Ancestor
-func NCA[T any](root, n1, n2 *node[T]) *node[T] {
-	// traverse tree to n1 and save node path in slice s1
-	// traverse tree to n2 and save node path in slice s2
-	// compare these slices and return last common node
-
-	return nil
-}
-
-func n[T any](v T) *node[T]              { return &node[T]{v: v} }
+func n[T any](v T) *node[T]              { return &node[T]{val: v} }
 func (n *node[T]) l(l *node[T]) *node[T] { n.left = l; return n }
 func (n *node[T]) r(r *node[T]) *node[T] { n.right = r; return n }
 
+// NCA stands for Nearest Common Ancestor
+func NCA[T any](root, n1, n2 *node[T]) *node[T] {
+	if root == nil || n1 == nil || n2 == nil {
+		return nil
+	}
+	// 1. traverse tree to n1 and save node path in slice s1
+	// 2. traverse tree to n2 and save node path in slice s2
+	// 3. compare these slices and return last common node
+
+	// Outputs the way from target to root
+	// *Return value is for internal use only
+	var getPath func(root, target *node[T], sink func(n *node[T])) bool
+	getPath = func(root, target *node[T], sink func(n *node[T])) bool {
+		if root == nil {
+			return false
+		}
+		if root == target || getPath(root.left, target, sink) || getPath(root.right, target, sink) {
+			sink(root)
+			return true
+		}
+		return false
+	}
+
+	var s1, s2 []*node[T]
+	getPath(root, n1, func(n *node[T]) { s1 = append(s1, n) })
+	getPath(root, n2, func(n *node[T]) { s2 = append(s2, n) })
+
+	for i := 1; ; i++ {
+		if len(s1)-i < 0 {
+			return s1[0]
+		} else if len(s2)-i < 0 {
+			return s2[0]
+		}
+		if s1[len(s1)-i] != s2[len(s2)-i] {
+			return s1[len(s1)-i+1]
+		}
+	}
+}
+
 func main() {
 
-	it := n(10).
-		l(n(5).
-			l(n(2)).
-			r(n(7))).
-		r(n(15))
-
-	var s []*node[int]
-	getPath(it, it.left.right, func(n *node[int]) { s = append(s, n) })
-	for _, el := range s {
-		fmt.Println(el.v)
-	}
 }
